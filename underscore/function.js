a79d39208af303056d044531234871afb1bc3326
frankLife <cquptzw@gmail.com>
2014-07-26 18:16:07 +0800

    up to contains

--- underscore/function.js
+++ underscore/function.js
@@ -23,6 +23,9 @@ var Type = {
   },
   isArray: function(obj) {
     return Type._typeFactory('Array')(obj);
+  },
+  isFunction: function(obj) {
+    return Type._typeFactory('Function')(obj);
   }
 }
 var Tool = {
@@ -176,10 +179,244 @@ function reduceRight(list, iterator, memo, context) {
       }
     }
   }else {
-    throw 'context: ' + context + '不是对象类型，无法绑定';
+    throw 'list: ' + list + '不是对象类型，无法绑定';
     return;
   }
 
   return _result;
+}
+function find(list, predicate, context) {
+  if(context) {
+    if(Type.isObj(context)) {
+      predicate = predicate.bind(context);
+    }else {
+      throw 'context' + context +'不是对象类型，无法绑定'
+      return;
+    }
+  }
+  if(Type.isObj(list)) {
+    for(item in list) {
+      if(predicate(list[item])) {
+        return list[item];
+      }
+    }
+    return undefined;
+  }else if(Type.isArray(list)) {
+    for(var i = 0, len = list.length;i<len;i++) {
+      if(predicate(list[i])) {
+        return list[i];
+      }
+    }
+    return undefined;
+  }else {
+    throw 'lsit' + list + '类型出错';
+  }
+}
+function filter(list, predicate, context) {
+  if(context) {
+    if(Type.isObj(context)) {
+      predicate = predicate.bind(context);
+    }else {
+      throw 'context: ' + context + '不是对象类型，无法绑定';
+    }
+  }
+  var _results = [];
+  if(Type.isObj(list)) {
+    for(var item in list) {
+      if(predicate(list[item])) {
+        _results.push(list[item]);
+      }
+    }
+    return _results;
+  }else if(Type.isArray(list)) {
+    for(var i = 0, len = list.length;i<len;i++) {
+      if(predicate(list[i])) {
+        _results.push(list[i]);
+      }
+    }
+    return _results;
+  }else {
+    throw 'list ' + list + '类型出错';
+  }
+
+}
+function where(list, props) {
+  var _results = [];
+  if(Type.isArray(list)&&Type.isObj(props)) {
+    for(var i = 0, len = list.length;i<len;i++) {
+      var flag = true;
+      for(var item in props) {
+        if(!(list[i].hasOwnProperty(item)&&list[i][item] == props[item])){
+          flag = false;
+          break;
+        }
+      }
+      if(flag) {
+        _results.push(list[i]);
+      }
+    }
+  }else {
+    throw 'list' + list + '类型不为数组或props' + props + '类型不为对象' ;
+  }
+
+  return _results;
+}
+function findWhere(list, props) {
+  if(Type.isArray(list)&&Type.isObj(props)) {
+    for(var i = 0, len = list.length;i<len;i++) {
+      var flag = true;
+      for(var item in props) {
+        if(!(list[i].hasOwnProperty(item)&&list[i][item] == props[item])){
+          flag = false;
+          break;
+        }
+      }
+      if(flag) {
+        return list[i];
+      }
+    }
+  }else {
+    throw 'list' + list + '类型不为数组或props' + props + '类型不为对象' ;
+  }
+}
+function reject(list, predicate, context) {
+  if(context) {
+    if(Type.isObj(context)) {
+      predicate = predicate.bind(context);
+    }else {
+      throw 'context' + context +'不是对象类型，无法绑定'
+      return;
+    }
+  }
+  var _results = [];
+  if(Type.isObj(list)) {
+    for(var item in list) {
+      if(!predicate(list[item])) {
+        _results.push(list[item]);
+      }
+    }
+    return _results;
+  }else if(Type.isArray(list)) {
+    for(var i = 0, len = list.length;i<len;i++) {
+      if(!predicate(list[i])) {
+        _results.push(list[i]);
+      }
+    }
+    return _results;
+  }else{
+    throw 'list' + list + '类型出错';
+  }
+}
+function every(list,predicate,context) {
+  if(predicate) {
+    try{
+    if(Type.isFunction(predicate)) {
+          if(reject(list,predicate,context).length > 0) {
+            return false;
+          }else {
+            return true;
+          }
+        }else {
+        throw 'predicate ' + predicate + '类型出错';
+      }
+    }
+    catch(e){
+        throw '参数传递错误';
+    }
+  }else {
+    if(Type.isObj(list)) {
+      for(var item in list) {
+        if(!list[item]) {
+          return false;
+        }
+      }
+      return true;
+    }else if(Type.isArray(list)) {
+      for(var i=0, len = list.length;i<len;i++) {
+        if(!list[i]) {
+          return false;
+        }
+      }
+      return true;
+    }else {
+      throw 'list ' + list + '类型出错';
+    }
+  }
+
+}
+function some(list,predicate,context) {
+  if(predicate) {
+    try {
+    if(Type.isFunction(predicate)) {
+      if(filter(list,predicate,context).length > 0) {
+        return true;
+      }else {
+        return false;
+      }
+    }else{
+      throw 'predicate ' + predicate + '类型出错';
+    }
+  }catch(e) {
+    throw '参数传递错误';
+  }
+  }else{
+    if(Type.isObj(list)) {
+      for(var item in list) {
+        if(list[item]) {
+          return true
+        }
+      }
+      return false;
+    }else if(Type.isArray(list)) {
+      for(var i = 0,len = list.length;i<len;i++) {
+        if(list[i]) {
+          return true;
+        }
+      }
+      return false;
+    }else {
+      throw 'list ' + list +'类型出错';
+    }
+  }
+}
+function contains(list,value) {
+  if(Type.isArray(list)) {
+    for(var i = 0, len = list.length;i<len;i++) {
+      if(list[i] === value) {
+        return true
+      }
+    }
+    return false;
+  }else if(Type.isObj(list)) {
+    for(var item in list) {
+      if(list[item] == value) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+function has(obj, key) {
+  if(Type.isObj(obj)) {
+    for(var item in obj) {
+      if(item == key) {
+        return true;
+      }
+    }
+    return false;
+  }else {
+    throw 'obj ' + obj + '类型不为对象';
+  }
+}
+function bind(func, obj, arg) {
+  if(obj == undefined) {
+    throw 'obj' + obj + '不能为空';
+    return;
+  }
+  return function() {
+    //这里函数的调用参数是通过bind来传递的
+    //如果需要通过返回的绑定的函数来自己传递参数，这吧参数写在包裹函数上
+    func.apply(obj,arg);
+  }
 }
 
--- underscore/test.js
+++ underscore/test.js
@@ -52,3 +52,87 @@ console.log(reduceRight({
   step1: [3,4]
 },testReduceRight));
 */
+function testBind(par,par2) {
+  console.log('par: ' + par);
+  console.log('par2: ' + par2);
+  console.log('name: ' + this.name);
+}
+/*
+bind(testBind,eachObj,['hello', 'world'])();
+*/
+
+function testFind(item) {
+  return item == this.name;
+}
+/*
+console.log(find(['Frank','FrankLife','Life'], testFind, eachObj));
+console.log(find({name: 'Frank',fullname: 'FrankLife'}, testFind, eachObj));
+*/
+function testFilter(item) {
+  return item % 2 == 0;
+}
+/*
+console.log(filter([1,2,4],testFilter));
+console.log(filter({step1: 1,step2: 2,step3: 4},testFilter));
+*/
+function testHas() {
+  console.log(has({name: '1'},'name'));
+}
+/*
+testHas();
+*/
+function testWhere() {
+  console.log(where([
+    {name: 'frank',year:'2014',test: '1'},
+    {name: 'lll',year: '2014',test: '2'},
+    {name:'frank',year:'2013',test:'3'}]
+    , {name: 'frank',year:'2014'}));
+}
+/*
+testWhere();
+*/
+function testFindWhere() {
+    console.log(findWhere([
+    {name: 'frank',year:'2014',test: '1'},
+    {name: 'lll',year: '2014',test: '2'},
+    {name:'frank',year:'2014',test:'3'}]
+    , {name: 'frank',year:'2014'}));
+}
+/*
+testFindWhere();
+*/
+function testReject(item) {
+  return item % 2 == 0;
+}
+/*
+console.log(reject([1,2,4],testReject));
+console.log(reject({step1: 1,step2: 2,step3: 4},testReject));
+*/
+function testEvery(item) {
+  return item % 2 == 0;
+}
+/*
+console.log(every([2,4],testEvery));
+console.log(every([1,2,4],testEvery));
+console.log(every([1,true]));
+*/
+function testSome(item) {
+  return item % 2 == 0;
+}
+/*
+console.log(some([1,2,4],testSome));
+console.log(some({
+  step1: 1,
+  step2: 1
+},testSome));
+console.log(some({
+  step1: false,
+  step2: false
+}));
+console.log(some([false,null,false]));
+*/
+function testContains() {
+  console.log(contains([1,2,4],1));
+  console.log(contains({step1:1,step2:2},1));
+}
+testContains()
