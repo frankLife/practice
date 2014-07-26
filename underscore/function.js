6f59ed0fecf77640c5c0c956b966047ff02b4308
frankLife <cquptzw@gmail.com>
2014-07-26 10:52:37 +0800

    map

--- underscore/function.js
+++ underscore/function.js
@@ -24,12 +24,23 @@ var Type = {
   isArray: function(obj) {
     return Type._typeFactory('Array')(obj);
   }
-
 }
-
+var Tool = {
+  makeArray: function(obj) {
+    return Array.prototype.slice.call(obj);
+  }
+}
+//TODO:兼容原生
 function each(list,iterator,context) {
-  if(Type.isObj(context)) {
+  if(!list || !iterator) {
+    throw "参数传递错误";
+    return;
+  }
+  if(context&&Type.isObj(context)) {
    iterator = iterator.bind(context);
+  }else {
+    throw context + '不是对象类型，无法绑定';
+    return;
   }
   if(Type.isObj(list)) {
     for(var item in list) {
@@ -40,7 +51,27 @@ function each(list,iterator,context) {
       iterator(list[i], i, list);
     }
   }else {
-    throw list+"类型出错."
+    throw list+"类型出错";
   }
 }
+function map(list,iterator,context) {
+  var _results = [];
+  if(context&&Type.isObj(context)) {
+    iterator = iterator.bind(context);
+  }
+  if(Type.isObj(list)) {
+    for(var item in list) {
+      _results[_results.length] = iterator(list[item],item);
+    }
+  }else if(Type.isArray(list)) {
+    for(var i = 0, len = list.length;i<len;i++) {
+      _results[_results.length] = iterator(list[i],i);
+    }
+  }else {
+    throw list+"类型出错";
+  }
+
+  return _results;
+}
+
 
--- underscore/test.js
+++ underscore/test.js
@@ -7,8 +7,27 @@ function testEach(value,key,list) {
 var eachObj = {
   name: 'FrankLife'
 };
+/*
 each({
   one: 'step1',
   two: 'step2'
 },testEach);
 each([2,3,4],testEach,eachObj);
+*/
+
+function testMakeArray () {
+  console.log(Tool.makeArray(arguments));
+  console.log(Type.isArray(Tool.makeArray(arguments)));
+}
+function testMap(value,key) {
+  console.log('this: ' + this.name);
+  console.log('value: ' + value);
+  console.log('key: ' + key);
+  return value * 2;
+}
+
+console.log(map([2,3,4], testMap, eachObj));
+console.log(map({
+  step1: 1,
+  step2: 2
+},testMap));
