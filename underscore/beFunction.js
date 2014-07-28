6c329a7262df7bb60b4f24ad1f9cf84f788233f3
上坡 <shangpo.zw@alibaba-inc.com>
2014-07-28 21:04:08 +0800

    better

---
+++ underscore/beFunction.js
@@ -0,0 +1,11 @@
+function bindFactory(func, context) {
+	if(Type.isObj(context)) {
+		return func.bind(context);
+	}
+}
+function _map(list,iterator,context) {
+	var _results = [];
+	each(list, function(){
+
+	});
+}
