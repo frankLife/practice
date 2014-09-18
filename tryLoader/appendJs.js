313173ce87d968aa051c525a682ee902821f6206
frankLife <cquptzw@gmail.com>
2014-09-18 22:55:49 +0800

    debug

--- tryLoader/appendJs.js
+++ tryLoader/appendJs.js
@@ -1,14 +1,15 @@
 function appendJs(name) {
   var script = document.createElement('script');
   script.src = './' + name + '.js';
- 
+
   var head = document.head || document.getElementsByTagName('head')[0];
   head.appendChild(script);
+
   script.onload = function(){
-	alert('ok');
+	console.log('ok');
     head.removeChild(script);
   }
-  
+
 }
 appendJs('a');
 appendJs('b');
