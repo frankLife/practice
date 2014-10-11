d8eb9f4cd2846eb3bb92f534106c49c9c5ac041f
上坡 <cquptzw@gmail.com>
2014-10-11 14:00:11 +0800

    flip

--- threeXD/flipAndSlide/index.js
+++ threeXD/flipAndSlide/index.js
@@ -1,9 +1,11 @@
 function bindFlip(){
   var trigger = document.getElementById('flipTrigger');
+  var trigger2 = document.getElementById('flipTrigger2');
   var flip = document.getElementById('flip');
 
   flip.style.transform = "";
   trigger.addEventListener('click',function(){
+    flip.style.transformOrigin = '50%'
     if(flip.style.transform == '') {
       flip.style.transform = 'rotateY(180deg)';
     }else {
@@ -11,5 +13,17 @@ function bindFlip(){
     }
     
   });
+  trigger2.addEventListener('click', function(){
+    console.log(flip.style.transform);
+    flip.style.transformOrigin = '100%'
+    if(flip.style.transform == '') {
+      flip.style.transform = 'rotateY(-180deg) translateX(300px)';
+    //   flip.style.transformOrigin = '50%'
+    }else {
+      flip.style.transform = "";
+    //  flip.style.transformOrigin = '50%'
+    }
+  });
+
 }
 bindFlip();
