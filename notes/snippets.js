def2c5e081a54345a085c9784adb77083d26803b
frankLife <cquptzw@gmail.com>
2014-12-16 23:24:32 +0800

    update

--- notes/snippets.js
+++ notes/snippets.js
@@ -12,3 +12,20 @@
   console.log('largest: ',largest(testArray));
 })();
 
+
+/*use native method width change the context to do work*/
+(function(){
+  var elems = {
+    length: 0,
+    add: function(elem){
+      Array.prototype.push.call(this,elem);
+    },
+    find: function(tagName){
+      this.add(document.getElementsByTagName(tagName)[0]);
+    }
+  }
+  console.log('before find elmes: ',elems);
+  elems.find('body');
+  elems.find('div');
+  console.log('after find elems: ',elems);
+})()
