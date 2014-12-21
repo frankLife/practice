cea338082f421ee325984f6a9ea1a36ce3c8c410
上坡 <cquptzw@gmail.com>
2014-12-21 13:50:09 +0800

    update

--- back/README.md
+++ back/README.md
@@ -1,5 +1,11 @@
 **Notes**
 
 0.  浏览器请求网页包含两个请求，请求页面文件本身，请求页面favico
-1.  If you attach a data event listener, then it will switch the stream into flowing mode, and data will be passed to your handler as soon as it is available. (FROM API) 通过stream绑定data时间来进行文件读取
+1.  If you attach a data event listener, then it will switch the stream into flowing mode, and data will be passed to your handler as soon as it is available. (FROM API) 通过stream绑定data事件来进行文件读取
+3.windows的cmd输入单个字符立马触发data事件，并且在c.write(data)时，将输入的原始字符再打印出来
+4.c.pipe(c) === c.on('data',function(data){c.write(data)})
+
+?:
+0.net 模块的clinet 的connect事件触发   ->不是通过server端建立的时候在异步回调的client中绑定，是在client端，通过connect方法建立的client中绑定,server端可以通过connection事件
+
 
--- back/netEvent.js
+++ back/netEvent.js
@@ -1,11 +1,77 @@
+/*net moudle event sample*/
+
+// var net = require('net');
+
+// var server = net.createServer(function(c){
+//   // c.on('data',function(data) {
+//   //   console.log(data.toString());
+//   //   c.write(data);
+//   // })
+//   c.pipe(c);  //c.pipe(c) === c.on('data',function(data){c.write(data)})
+//   // c.on('connect',function(){
+//   //   console.log(this.remoteAddress);
+//   // });
+// });
+// server.on('connection',function(c){
+//   var id = c.remoteAddress + ':' + c.remotePort;
+//   console.log("c.remoteAddress: ",c.remoteAddress);
+//   console.log("c.remotePort: ",c.remotePort);
+
+// });
+// server.listen(3000,function(){
+//   console.log('server liten on port 3000');
+// });
+
+/* create client */
+// var client = net.connect({port: 3000,host: 'localhost'},
+//   function() { //'connect' listener
+//   console.log('connected to server!');
+//   client.write('world!\r\n');
+// });
+// client.on('connect',function(client){
+//   console.log('client connect');
+// });
+// client.on('data', function(data) {
+//   console.log('client: '+ data.toString());
+//   // client.end();
+// });
+// client.on('end', function() {
+//   console.log('disconnected from server');
+// });
+
+
+/* pubsub.js  create broadcast server using net module*/
+
 var net = require('net');
+var events = require('events');
+
+var channel = new events.EventEmitter();
+channel.clients = {};
+channel.subscriptions = {};
+
+channel.on('join',function(id,c){
+  this.clients[id] = c;
+  this.subscriptions[id] = function(senderId,data) {
+    if(id != senderId) {
+      this.clients[id].write(data.toString());
+    }
+  }
+  //bind e event each time when a client join in,
+  // when broadcast event emitted bind handdlers excute sequencely
+  this.on('broadcast',this.subscriptions[id]);
+});
 
 var server = net.createServer(function(c){
-  c.on('data',function(data) {
-    c.write(data);
-  })
+  var id = c.remoteAddress + ':' + c.remotePort;
+
+  // c.on('connection',function(){
+    console.log(c.remoteAddress + ':' + c.remotePort);
+    channel.emit('join',id,c);
+  // });
+  c.on('data',function(data){
+    channel.emit('broadcast',id,data);
+  });
+}).listen(3000,function(){
+  console.log('listen on port 3000');
 });
 
-server.listen(3000,function(){
-  console.log('server liten on port 3000');
-});
