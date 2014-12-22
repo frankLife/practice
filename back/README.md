3af4876e49e0d946785b9205bd2685907dfb2e51
上坡 <cquptzw@gmail.com>
2014-12-22 09:35:29 +0800

    update

--- back/README.md
+++ back/README.md
@@ -6,7 +6,7 @@
 4.	c.pipe(c) === c.on('data',function(data){c.write(data)})
 5.	终端输入的时机，cmd是输入任何字符就会发送消息，xshell输入时发送输入内容之后再发送一次0d0a
 6.  util.inherits(ChildClass,SuperClass) 相当于  ChildClass.prototype =  new SuperClass(),如果需要继承属性的话，还是需要SuperClass.call(this,[parameters]);
-
+7.  rename可以通过指定不同的文件夹进行文件移动(剪切)以及本身可以进行重命名
 ?:
 0.net 模块的clinet 的connect事件触发   ->不是通过server端建立的时候在异步回调的client中绑定，是在client端，通过connect方法建立的client中绑定,server端可以通过connection事件
 
---
+++ back/watchFile/fileWatcher.js
@@ -0,0 +1,43 @@
+var events = require('events');
+var util = require('util');
+var fs = require('fs');
+
+function Watcher(opt){
+  events.EventEmitter.call(this);
+  this.watchDir = opt.watchDir;
+  this.doneDir = opt.doneDir;
+}
+util.inherits(Watcher,events.EventEmitter);
+Watcher.prototype.watch = function(){
+  var self = this;
+  console.log('this.watchDir: ',this.watchDir);
+  fs.watchFile(this.watchDir,function(){
+    console.log('watch emitted!');
+    self.emit('process');
+  });
+}
+
+
+
+
+var watcher = new Watcher({watchDir: './watch',doneDir:'./done'});
+
+watcher.on('process',function(){
+  var self = this;
+  fs.readdir(this.watchDir,function(err,files){
+    if(err) {
+      console.log(err);
+      return;
+    }
+    console.log('files: ',files);
+    for(var i = 0,len = files.length;i<len;i++) {
+      console.log(files[i]);
+      // /* cut file from watch directory to done directory */
+      // fs.rename(self.watchDir + '/' + files[i],self.doneDir + '/' + files[i].toUpperCase());
+      fs.createReadStream(self.watchDir + '/' + files[i] ).pipe( fs.createWriteStream(self.doneDir + '/' + files[i].toUpperCase()));
+    }
+  });
+});
+
+
+watcher.watch();
