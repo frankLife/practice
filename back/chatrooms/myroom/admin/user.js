06f57b3efd51d119e0c2afdf942349b47e389f48
frankLife <cquptzw@gmail.com>
2015-01-18 15:43:29 +0800

    init admin

---
+++ back/chatrooms/myroom/admin/user.js
@@ -0,0 +1,30 @@
+var MongoClient = require('mongodb').MongoClient;
+
+function initConnect(){
+  MongoClient.connect('mongodb://localhost:27017/myroom',function(err,openDB){
+    if(err) {
+      return err;
+    }else {
+      console.log('db connect success');
+    }
+
+    insertUser(openDB,
+    {
+      userId:2,
+      username:'tom',
+      password:'1',
+      friends: ['franklife']
+    });
+  });
+}
+function insertUser(db,userData){
+  var userCol = db.collection('user');
+  userCol.insert(userData,function(err,result){
+    if(err) {
+      throw err;
+    }
+    console.log('insert result: ');
+    console.log(result);
+  });
+}
+initConnect();
