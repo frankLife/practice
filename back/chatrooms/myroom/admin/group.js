5691e44632f0239eef282348364f8a2f6f9e5a97
frankLife <cquptzw@gmail.com>
2015-01-21 23:58:15 +0800

    update

---
+++ back/chatrooms/myroom/admin/group.js
@@ -0,0 +1,28 @@
+var MongoClient = require('mongodb').mongoClient;
+
+function initConnect(){
+  MongoClient.connect('mongodb://localhost:27017/myroom',function(err,db){
+    if(err) {
+      throw err;
+    }else {
+      console.log('db connect successfully');
+    }
+
+    insertGroup(db,{
+      groupId:1,
+      members: ['franklife','tom']
+    })
+  });
+}
+function insertGroup(db,groupData){
+  var groupCol = db.collection('group');
+  groupCol.insert(groupData,function(err,result){
+    if(err) {
+      throw err;
+    }
+    console.log('insert result');
+    console.log(result);
+  });
+}
+
+initConnect();
