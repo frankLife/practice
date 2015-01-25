5af481e71b0c80d586f86fdb79646027ac5ed37b
frankLife <cquptzw@gmail.com>
2015-01-25 22:54:00 +0800

    update

--- back/network/netClient.js
+++ back/network/netClient.js
@@ -5,15 +5,20 @@ var client = net.connect({port: 3000,host: 'localhost'});
 client.on('connect',function(){
   console.log('connected to server!');
   client.write('world!\r\n');
+  /*
   setInterval(function(){
     client.write('sendTimes: '+ (++sendTimes));
   },1000);
+  */
+  process.stdin.pipe(client);  
   console.log('client connect');
 });
+/*
 client.on('data', function(data) {
   console.log('client: '+ data.toString());
   // client.end();
 });
+*/
 client.on('end', function() {
   console.log('disconnected from server');
 });
