ba40593760f0b7ab31e42c9ed3dad38c825baf35
frankLife <cquptzw@gmail.com>
2015-01-26 23:35:03 +0800

    update

--- back/express/downTest/app.js
+++ back/express/downTest/app.js
@@ -27,6 +27,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', routes);
 // app.use('/users', users);
+app.use(routes);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- back/express/downTest/routes/route.js
+++ back/express/downTest/routes/route.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get(/\/|\/about/,function(){
-  
+router.get(/\/|\/about/,function(req,res,next){
+  res.render('about',{title:'Photo'});
 });
+
+module.exports = router;
