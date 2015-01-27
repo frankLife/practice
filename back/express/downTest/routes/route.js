43eff6a6ccf6aad2615ef5c7aef3b5e9d80a0346
frankLife <cquptzw@gmail.com>
2015-01-28 00:40:53 +0800

    upload

--- back/express/downTest/routes/route.js
+++ back/express/downTest/routes/route.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-router.get(/\/|\/about/,function(req,res,next){
+
+router.get(/^\/$|^\/about$/,function(req,res,next){
   res.render('about',{title:'Photo'});
 });
+router.get('/upload',function(req,res,next){
+  res.render('upload');
+});
+router.post('/upload',function(req,res,next){
 
+});
 module.exports = router;
