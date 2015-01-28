e1c347d35163cd2688bf635a528adc91bec86393
frankLife <cquptzw@gmail.com>
2015-01-28 23:29:24 +0800

    update upload express

---
+++ back/express/downTest/cut.js
@@ -0,0 +1,33 @@
+var fs = require('fs');
+var path = require('path');
+
+function cut(oldPath,newPath,isKeepExt,cb) {
+  if(isKeepExt) {
+    if(path.extname(oldPath) != path.extname(newPath)) {
+      throw new Error('file extension name should be keep the same');
+    }
+  }
+  fs.rename(oldPath,newPath,function(err){
+    if(err && err.code == 'EXDEV') {
+      transport(oldPath,newPath,cb);
+      return;
+    }
+    cb && cb();
+  });
+}
+function transport(oldPath,newPath,cb){
+  var readStream = fs.createReadSteam(oldPath);
+  var writeStream = fs.createWriteStream(newPath);
+
+  readStream.pipe(writeStream);
+  readStream.on('close',function(){
+    fs.unlink(oldPath,function(err){
+      if(err) {
+        throw err;
+      }
+      cb && cb();
+    });
+  });
+}
+
+exports.cut = cut;
--- back/express/downTest/routes/route.js
+++ back/express/downTest/routes/route.js
@@ -1,14 +1,44 @@
 var express = require('express');
 var router = express.Router();
-
+var multiparty = require('multiparty');
+var mongoose = require('mongoose');
+var util = require('util');
+var path = require('path');
+var cut = require('../cut');
+var root = __dirname;
+mongoose.connection.on('open',function(){
+  console.log('mongodb connected successfully!');
+})
+mongoose.connect('mongodb://localhost:27017/mtest');
+var photoSchema = mongoose.Schema({
+  name: String,
+  path: String
+});
+var Photo = mongoose.model('Photo',photoSchema);
 
 router.get(/^\/$|^\/about$/,function(req,res,next){
   res.render('about',{title:'Photo'});
 });
 router.get('/upload',function(req,res,next){
-  res.render('upload');
+  res.render('upload',{title:'upload'});
 });
 router.post('/upload',function(req,res,next){
+  var form = new multiparty.Form();
+  form.parse(req,function(err,fields,files) {
+    if(err) {
+      throw err;
+    }
+    // console.log(util.inspect({fields: fields, files: files}))
+    console.dir(files);
+    var dirPath = path.join(root,'/ignore/',fields.photoName);
+    cut.cut(files.photoImages[0].path,dirPath,false,function(){
+      var tempPhoto = new Photo({name:fields.photoName,path:dirPath});
+      console.log('upload success: ');
+      console.log(tempPhoto.name);
+      console.log(tempPhoto.path);
+    });
+    res.redirect('/upload');
+  });
 
 });
 module.exports = router;
